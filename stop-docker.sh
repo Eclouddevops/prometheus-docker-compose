980d76801cd01084785c051d815234b6dbe5c3d8
Eclouddevops <eclouddevops@gmail.com>
2025-09-22 22:08:27 +0530

    addfiles

--- stop-docker.sh
+++ stop-docker.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 
 docker stop $(docker ps -q) && docker rm $(docker ps -a -q) && docker image prune -a -f
-docker stop $(docker ps -q)
 
 docker ps -q | xargs -r docker stop
 
- docker ps -a -q | xargs -r docker rm
+docker ps -a -q | xargs -r docker rm
 
  docker image prune -a -f
 
